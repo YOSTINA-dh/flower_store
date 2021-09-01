3ce6732bd27d5acd0b958a4db388a44c8a56ece4
YOSTINA-dh <yostinadesta@gmail.com>
2021-09-01 22:05:58 +0000

    Uppdating base.html

--- flower_store/settings.py
+++ flower_store/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
